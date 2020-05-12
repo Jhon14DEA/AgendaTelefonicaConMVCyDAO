ed45c0a12ebd289926ed30828447b0bc2506e85f
JHON <JHON@DESKTOP-K1VJ8NK>
2020-05-12 09:21:59 -0500

    Creacion delos paquetes y las clases Usuario y ICliente.

---
+++ src/ec/edu/ups/controlador/ControladorUsuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import ec.edu.ups.idao.IClienteDAO;
+import ec.edu.ups.modelo.Usuario;
+
+/**
+ *
+ * @author JHON
+ */
+public class ControladorUsuario {
+    
+    private Usuario Usuario;
+    public IClienteDAO clienteDAO;
+    
+    public void registrar (){
+    //Usuario =
+    }
+    
+}
