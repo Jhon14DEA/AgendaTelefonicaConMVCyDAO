ad7fa0efc3935c2df6247cb9a27237249dbca931
JHON <JHON@DESKTOP-K1VJ8NK>
2020-05-15 09:40:53 -0500

    Creacion delos paquetes y las clases Usuario y ICliente.

--- src/ec/edu/ups/modelo/Usuario.java
+++ src/ec/edu/ups/modelo/Usuario.java
@@ -137,10 +137,10 @@ public class Usuario {
     @Override
     public String toString() {
         
-        return "Usuario{" + "cedula=" + cedula +
-                ", nombre=" + nombre + ", apellido=" 
-                + apellido + ", correo=" + correo +
-                ", contrase\u00f1a=" + contraseña +
+        return "USUARIO\n" + "cedula:  " + cedula +
+                "\nnombre: " + nombre + "\napellido: " 
+                + apellido + "\ncorreo: " + correo +
+                "\ncontrase\u00f1a: " + contraseña +
                 '}'+ "\nTelefonos: " + telefonos.toString();
     }
 
